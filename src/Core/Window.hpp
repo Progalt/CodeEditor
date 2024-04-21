2f61753042f7e647c87ed08bb32d0785989150e0
Progalt <henryrgronow@gmail.com>
2024-04-21 14:38:56 +0100

    Starting Window.hppo

---
+++ src/Core/Window.hpp
@@ -0,0 +1,22 @@
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <SDL2/SDL.h>
+
+namespace editor
+{
+	class Window
+	{
+	public:
+	private:
+
+		SDL_Window* m_Window; 
+
+
+		SDL_GLContext m_Context;
+
+	};
+}
+
+#endif // WINDOW_H 
